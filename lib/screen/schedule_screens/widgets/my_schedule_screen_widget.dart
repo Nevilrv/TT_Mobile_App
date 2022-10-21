291fb1a461003475f0707942f3816b3923161ac1
Dam Code <damcode1990@gmail.com>
2022-10-21 11:45:21 +0530

    21-10-22

--- lib/screen/schedule_screens/widgets/my_schedule_screen_widget.dart
+++ lib/screen/schedule_screens/widgets/my_schedule_screen_widget.dart
@@ -270,7 +270,7 @@ void openBottomSheet(
                 print(
                     'supersetRound >>> ${_userWorkoutsDateViewModel.userProgramDatesId}');
 
-                return /*scheduleByDateViewModel!.completeDate
+                return scheduleByDateViewModel!.completeDate
                         .contains(date.toString().split(' ').first)
                     ? TextButton(
                         onPressed: null,
@@ -278,8 +278,7 @@ void openBottomSheet(
                           "This workout is Completed",
                           style: FontTextStyle.kTint24W400Roboto,
                         ))
-                    : */
-                    snapshot.data!.data!.exercisesIds!.isEmpty ||
+                    : snapshot.data!.data!.exercisesIds!.isEmpty ||
                             snapshot.data!.data!.exercisesIds == []
                         ? TextButton(
                             onPressed: null,
