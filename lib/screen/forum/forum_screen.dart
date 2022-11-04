6c1741c6daf08fba91a05f1a1f62f81e5c55c788
Dam Code <damcode1990@gmail.com>
2022-11-04 13:43:57 +0530

    21-10-22

--- lib/screen/forum/forum_screen.dart
+++ lib/screen/forum/forum_screen.dart
@@ -820,7 +820,7 @@ class _ForumScreenState extends State<ForumScreen> {
                 onTap: () async {
                   if (response!.data![index].userDisliked == 0) {
                     if (controller.likeDisLike[index].userLiked == 0) {
-                      controller.likeDisLike[index].userDisliked = 1;
+                      controller.likeDisLike[index].userDisliked = 1;K
                       DisLikeForumRequestModel model =
                           DisLikeForumRequestModel();
                       model.postId = response.data![index].postId;
